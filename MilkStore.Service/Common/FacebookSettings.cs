a13b8510a57938495a6757e95e16e2ddc987ccb8
i'm tum <minhtam250102@gmail.com>
2024-06-23 23:35:50 +0700

    config facebook settings

--- MilkStore.Service/Common/FacebookSettings.cs
+++ MilkStore.Service/Common/FacebookSettings.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace MilkStore.Service.Common
 {
-    public class AuthenticationSettings
+    public class FacebookSettings
     {
-        public GoogleSettings Google { get; set; }
-        public FacebookSettings Facebook { get; set; }
+        public string AppId { get; set; }
+        public string AppSecret { get; set; }
     }
 }
