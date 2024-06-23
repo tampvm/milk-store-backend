c4ef44ad11b6114ff9891cb006904d55c2cf7eb0
i'm tum <minhtam250102@gmail.com>
2024-06-23 23:26:39 +0700

    add google service into project

---
+++ MilkStore.Service/Interfaces/IGoogleSerive.cs
@@ -0,0 +1,15 @@
+﻿using MilkStore.Service.Models.ViewModels.AuthViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkStore.Service.Interfaces
+{
+    public interface IGoogleSerive
+    {
+        Task<GoogleTokenResponse> ExchangeAuthCodeForTokensAsync(string authCode);
+        Task<GooglePayload> VerifyGoogleTokenAsync(string token);
+    }
+}
