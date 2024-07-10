c5412e62e24be29d94dbebfa9ed5ac460ae8bcb9
uyentramle <uyentramle1010@gmail.com>
2024-07-10 14:38:42 +0700

    get all vouchers function

---
+++ MilkStore.API/Controllers/VoucherController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MilkStore.Service.Interfaces;
+
+namespace MilkStore.API.Controllers
+{
+	public class VoucherController : BaseController
+	{
+		private readonly IVoucherService _voucherService;
+
+		public VoucherController(IVoucherService voucherService)
+		{
+			_voucherService = voucherService;
+		}
+
+		#region Voucher management
+		// Get all vouchers
+		[HttpGet]
+		public async Task<IActionResult> GetVouchersAsync(int pageIndex = 0, int pageSize = 10)
+		{
+			var vouchers = await _voucherService.GetVouchersAsync(pageIndex, pageSize);
+			return Ok(vouchers);
+		}
+		#endregion
+	}
+}
---
+++ MilkStore.Service/Services/VoucherService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MilkStore.Repository.Common;
+using MilkStore.Repository.Interfaces;
+using MilkStore.Service.Interfaces;
+using MilkStore.Service.Models.ResponseModels;
+using MilkStore.Service.Models.ViewModels.VoucherViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkStore.Service.Services
+{
+	public class VoucherService : IVoucherService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IMapper _mapper;
+
+		public VoucherService(IUnitOfWork unitOfWork, IMapper mapper)
+		{
+			_unitOfWork = unitOfWork;
+			_mapper = mapper;
+		}
+
+		#region Voucher management
+		// Get all vouchers
+		public async Task<ResponseModel> GetVouchersAsync(int pageIndex, int pageSize)
+		{
+			var vouchers = await _unitOfWork.VoucherRepository.GetAsync(
+				pageIndex: pageIndex,
+				pageSize: pageSize
+				);
+
+			var voucherDtos = _mapper.Map<Pagination<ViewListVoucherDTO>>(vouchers);
+
+			return new SuccessResponseModel<object>
+			{
+				Success = true,
+				Message = "Vouchers retrieved successfully.",
+				Data = voucherDtos
+			};
+		}
+
+		#endregion
+	}
+}
